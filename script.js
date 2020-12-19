fd58feb2397ddcf1e8ce2d654c80dac2512c2e89
Zarin Salim <zarin_96@hotmail.co.uk>
2020-12-19 23:25:31 +0000

    created a blank array for the other arrays to be stored in depending on user prompts

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numerical = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "=", "?", "@", "["];
+var possibleChars = []
 
 // Prompt to ask how many characters for the password
 function generatePassword() {
@@ -27,44 +28,37 @@ function generatePassword() {
     var confirmLower = confirm("Click OK to confirm including lowercase letters");
     var confirmNumber = confirm("Click OK to confirm including numeric characters");
     var confirmSpecial = confirm("Click OK to confirm including special characters");
-    var choices = {
-      amount,
-      confirmUpper,
-      confirmLower,
-      confirmSpecial,
-      confirmNumber
-    };
+  
 
     if (confirmUpper) {
       for (i = 0, i < upper.length; i++;) {
-        choices.push(upper[i]);
+        possibleChars.push(upper[i]);
       }
     }
 
     if (confirmLower) {
       for (i = 0, i < lower.length; i++;) {
-        choices.push(lower[i]);
+        possibleChars.push(lower[i]);
       }
     }
 
     if (confirmNumber) {
       for (i = 0, i < numerical.length; i++;) {
-        choices.push(numerical[i]);
+        possibleChars.push(numerical[i]);
       }
     }
 
-
     if (confirmSpecial) {
       for (i = 0, i < special.length; i++;) {
-        choices.push(special[i]);
+        possibleChars.push(special[i]);
       };
-
-      console.log("confirmUpper ", confirmUpper);
-      console.log("confirmLower ", confirmLower);
-      console.log("confirmNumber ", confirmNumber);
-      console.log("confirmSpecial ", confirmSpecial);
     }
 
+    console.log("confirmUpper ", confirmUpper);
+    console.log("confirmLower ", confirmLower);
+    console.log("confirmNumber ", confirmNumber);
+    console.log("confirmSpecial ", confirmSpecial);
+    console.log("possibleChars ", possibleChars)
 
 
 } else {
@@ -78,19 +72,10 @@ function generatePassword() {
 
 
 
-
-
-
-
-// console.log(choices);
-
 // // Write password to the #password input
 
 // // {password = choices.charAt(Math.floor(Math.random() * amount.length))
 // // }
-
-// var passChoices = JSON.stringify(choices);
-// var password = JSON.parse(passChoices)
 
 // var password = "";
 // for (i = 0; i < passLength; i++) {
